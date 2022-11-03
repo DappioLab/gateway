107f53ce16ab3a105914c6538c86d5a06eb82b36
emerson <emerson@dappio.xyz>
2022-11-03 20:31:01 +0000

    Update

--- tests/testAdapterGenopets.ts
+++ tests/testAdapterGenopets.ts
@@ -17,10 +17,13 @@ import {
 import { genopets } from "@dappio-wonderland/navigator";
 
 describe("Gateway", () => {
-  const connection = new Connection("https://rpc-mainnet-fork.epochs.studio", {
-    commitment: "confirmed",
-    wsEndpoint: "wss://rpc-mainnet-fork.epochs.studio/ws",
-  });
+  const connection = new Connection(
+    "https://rpc-mainnet-fork.epochs.studio/notcache",
+    {
+      commitment: "confirmed",
+      wsEndpoint: "wss://rpc-mainnet-fork.epochs.studio/ws",
+    }
+  );
   // const connection = new Connection("https://solana-api.tt-prod.net", {
   //   commitment: "confirmed",
   //   confirmTransactionInitialTimeout: 180 * 1000,
@@ -41,114 +44,110 @@ describe("Gateway", () => {
   anchor.setProvider(provider);
 
   const zapInAmount = 10000;
+  const GENE_USDC_LP = new PublicKey(
+    "7GKvfHEXenNiWYbJBKae89mdaMPr5gGMYwZmyC8gBNVG"
+  );
+  const GENE_MINT = new PublicKey(
+    "GENEtH5amGSi8kHAtQoezp1XEXwZJ8vcuePYnXdKrMYz"
+  );
 
-  // it("Swap + AddLiquidity + Stake in Genopets", async () => {
-  //   // const poolId = new anchor.web3.PublicKey(
-  //   //   "DPgYdwgz7ZytfrzLeRYJwXq9JGeskaXTRrM8biFEnLs1" // PRGC-USDC
-  //   // );
-  //   const farmId = new anchor.web3.PublicKey(
-  //     "tEAbLeDznDdQ5jvdk1cdm2qUzoYmyc6nX5FChAyAB2U" // GENE
-  //   );
-  //   const swapParams: SwapParams = {
-  //     protocol: SupportedProtocols.Jupiter,
-  //     fromTokenMint: new PublicKey(
-  //       // "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v" // USDC
-  //       "So11111111111111111111111111111111111111112" // WSOL
-  //     ),
-  //     toTokenMint: new PublicKey(
-  //       "GENEtH5amGSi8kHAtQoezp1XEXwZJ8vcuePYnXdKrMYz" // GENE
-  //     ),
-  //     amount: zapInAmount / 2, // Swap half of the fromToken to proceed zapIn
-  //     slippage: 1,
-  //   };
-  //   // const addLiquidityParams: AddLiquidityParams = {
-  //   //   protocol: SupportedProtocols.Genopets,
-  //   //   poolId,
-  //   // };
-  //   const farm = (await genopets.infos.getFarm(
-  //     connection,
-  //     farmId
-  //   )) as genopets.FarmInfo;
-  //   const stakeParams: StakeParams = {
-  //     protocol: SupportedProtocols.Genopets,
-  //     farmId,
-  //     version: 1,
-  //     lpAmount: 1000,
-  //     lockDuration: 0,
-  //   };
+  const mint = GENE_USDC_LP;
+  // const mint = GENE_MINT;
 
-  //   const gateway = new GatewayBuilder(provider);
+  it("Stake in Genopets", async () => {
+    const farmId = new anchor.web3.PublicKey(
+      "tEAbLeDznDdQ5jvdk1cdm2qUzoYmyc6nX5FChAyAB2U" // GENE
+    );
+    const swapParams: SwapParams = {
+      protocol: SupportedProtocols.Jupiter,
+      fromTokenMint: new PublicKey(
+        // "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v" // USDC
+        "So11111111111111111111111111111111111111112" // WSOL
+      ),
+      toTokenMint: new PublicKey(
+        "GENEtH5amGSi8kHAtQoezp1XEXwZJ8vcuePYnXdKrMYz" // GENE
+      ),
+      amount: zapInAmount / 2, // Swap half of the fromToken to proceed zapIn
+      slippage: 1,
+    };
+    // const addLiquidityParams: AddLiquidityParams = {
+    //   protocol: SupportedProtocols.Genopets,
+    //   poolId,
+    // };
+    const farm = (await genopets.infos.getFarm(
+      connection,
+      farmId
+    )) as genopets.FarmInfo;
+    const stakeParams: StakeParams = {
+      protocol: SupportedProtocols.Genopets,
+      farmId,
+      version: 1,
+      lpAmount: 10000,
+      lockDuration: 0,
+      mint,
+    };
 
-  //   // await gateway.swap(swapParams);
-  //   console.log(gateway.params.swapMinOutAmount.toNumber());
-  //   // Work-around
-  //   // addLiquidityParams.tokenInAmount =
-  //   //   gateway.params.swapMinOutAmount.toNumber();
-  //   // await gateway.addLiquidity(addLiquidityParams);
-  //   await gateway.stake(stakeParams);
+    const gateway = new GatewayBuilder(provider);
 
-  //   await gateway.finalize();
+    // await gateway.swap(swapParams);
+    console.log(gateway.params.swapMinOutAmount.toNumber());
+    // Work-around
+    // addLiquidityParams.tokenInAmount =
+    //   gateway.params.swapMinOutAmount.toNumber();
+    // await gateway.addLiquidity(addLiquidityParams);
+    await gateway.stake(stakeParams);
 
-  //   console.log(gateway.params);
-  //   // console.log(`swapInAmount: ${gateway.params.swapInAmount}`);
-  //   // console.log(`swapMinOutAmount: ${gateway.params.swapMinOutAmount}`);
+    await gateway.finalize();
 
-  //   const txs = gateway.transactions();
+    console.log(gateway.params);
+    // console.log(`swapInAmount: ${gateway.params.swapInAmount}`);
+    // console.log(`swapMinOutAmount: ${gateway.params.swapMinOutAmount}`);
 
-  //   console.log("======");
-  //   console.log("Txs are sent...");
-  //   for (let tx of txs) {
-  //     const sig = await provider.sendAndConfirm(tx, [], {
-  //       skipPreflight: true,
-  //       commitment: "confirmed",
-  //     } as unknown as anchor.web3.ConfirmOptions);
-  //     console.log(sig);
-  //   }
-  //   console.log("Txs are executed");
-  //   console.log("======");
-  // });
+    const txs = gateway.transactions();
 
-  it("Unstake + Harvest + RemoveLiquidity + Swap in Genopets", async () => {
-    // const poolId = new anchor.web3.PublicKey(
-    //   "GaqgfieVmnmY4ZsZHHA6L5RSVzCGL3sKx4UgHBaYNy8m" // RAY-SRM
-    // );
+    console.log("======");
+    console.log("Txs are sent...");
+    for (let tx of txs) {
+      const sig = await provider.sendAndConfirm(tx, [], {
+        skipPreflight: true,
+        commitment: "confirmed",
+      } as unknown as anchor.web3.ConfirmOptions);
+      console.log(sig);
+    }
+    console.log("Txs are executed");
+    console.log("======");
+  });
+
+  it("Unstake + Harvest in Genopets", async () => {
     const farmId = new anchor.web3.PublicKey(
       "tEAbLeDznDdQ5jvdk1cdm2qUzoYmyc6nX5FChAyAB2U"
     );
 
-    // const pool = await genopets.infos.getPool(connection, poolId);
-    // const poolWrapper = new genopets.PoolInfoWrapper(pool as genopets.PoolInfo);
     const farm = (await genopets.infos.getFarm(
       connection,
       farmId
     )) as genopets.FarmInfo;
 
-    // Get share amount
-    const ledgerKey = await genopets.infos.getFarmerId(
+    const farmerId = await genopets.infos.getFarmerId(
       farm,
       provider.wallet.publicKey
     );
     const farmer = (await genopets.infos.getFarmer(
       connection,
-      ledgerKey
+      farmerId
     )) as genopets.FarmerInfo;
-    const shareAmount = Number(farmer.totalRewards);
-    console.log(shareAmount);
 
-    // const { tokenAAmount, tokenBAmount } = await poolWrapper.getTokenAmounts(
-    //   shareAmount
-    // );
     const harvestParams1: HarvestParams = {
       protocol: SupportedProtocols.Genopets,
       farmId,
       type: HarvestType.ClaimRewards,
+      mint,
     };
 
     let depositId: anchor.web3.PublicKey;
     for (let deposit of farmer.userDeposit) {
       if (deposit?.isYield == true) {
         depositId = deposit.depositId;
-        break;
       }
     }
     const harvestParams2: HarvestParams = {
@@ -156,10 +155,17 @@ describe("Gateway", () => {
       farmId,
       type: HarvestType.WithdrawAsSgene,
       farmerKey: depositId!,
+      mint,
     };
 
     for (let deposit of farmer.userDeposit) {
-      if (deposit?.isYield == false) {
+      const timestamp = Number(deposit?.lockUntil);
+      const currentTimestamp = Number(new Date()) / 1000;
+      if (
+        deposit?.isYield == false &&
+        deposit.poolToken.equals(mint) &&
+        timestamp < currentTimestamp
+      ) {
         depositId = deposit.depositId;
       }
     }
@@ -167,14 +173,16 @@ describe("Gateway", () => {
       protocol: SupportedProtocols.Genopets,
       farmId,
       type: HarvestType.Withdraw,
-      farmerKey: new PublicKey("Epr3R75CTWeRWdB9sqBjj192dgJyAt4tvyZzPJtbbQNT"),
+      farmerKey: depositId!,
+      mint,
     };
-    // const unstakeParams: UnstakeParams = {
-    //   protocol: SupportedProtocols.Genopets,
-    //   farmId,
-    //   shareAmount,
-    //   version: 1,
-    // };
+    const unstakeParams: UnstakeParams = {
+      protocol: SupportedProtocols.Genopets,
+      farmId,
+      shareAmount: 1000, // dummy
+      farmerKey: depositId!,
+      mint,
+    };
     // const removeLiquidityParams: RemoveLiquidityParams = {
     //   protocol: SupportedProtocols.Raydium,
     //   poolId,
@@ -195,8 +203,8 @@ describe("Gateway", () => {
 
     // await gateway.harvest(harvestParams1);
     // await gateway.harvest(harvestParams2);
-    await gateway.harvest(harvestParams3);
-    // await gateway.unstake(unstakeParams);
+    // await gateway.harvest(harvestParams3);
+    await gateway.unstake(unstakeParams);
     // await gateway.removeLiquidity(removeLiquidityParams);
     // await gateway.swap(swapParams);
 
@@ -220,4 +228,138 @@ describe("Gateway", () => {
     console.log("Txs are executed");
     console.log("======");
   });
+
+  // it("Unstake all in Genopets", async () => {
+  //   const farmId = new anchor.web3.PublicKey(
+  //     "tEAbLeDznDdQ5jvdk1cdm2qUzoYmyc6nX5FChAyAB2U"
+  //   );
+
+  //   const farm = (await genopets.infos.getFarm(
+  //     connection,
+  //     farmId
+  //   )) as genopets.FarmInfo;
+
+  //   const farmerId = await genopets.infos.getFarmerId(
+  //     farm,
+  //     provider.wallet.publicKey
+  //   );
+  //   const farmer = (await genopets.infos.getFarmer(
+  //     connection,
+  //     farmerId
+  //   )) as genopets.FarmerInfo;
+
+  //   const gateway = new GatewayBuilder(provider);
+
+  //   const harvestParams1: HarvestParams = {
+  //     protocol: SupportedProtocols.Genopets,
+  //     farmId,
+  //     type: HarvestType.ClaimRewards,
+  //     mint,
+  //   };
+  //   await gateway.harvest(harvestParams1);
+
+  //   let depositId: anchor.web3.PublicKey;
+  //   let counter = 0;
+  //   for (let deposit of farmer.userDeposit) {
+  //     const timestamp = Number(deposit?.lockUntil);
+  //     const currentTimestamp = Number(new Date()) / 1000;
+  //     if (
+  //       deposit?.isYield == false &&
+  //       deposit.poolToken.equals(mint) &&
+  //       timestamp < currentTimestamp
+  //     ) {
+  //       depositId = deposit.depositId;
+
+  //       const unstakeParams: UnstakeParams = {
+  //         protocol: SupportedProtocols.Genopets,
+  //         farmId,
+  //         shareAmount: 1000, // dummy
+  //         farmerKey: depositId!,
+  //         mint,
+  //       };
+
+  //       await gateway.unstake(unstakeParams);
+  //       counter++;
+  //       if (counter == 1) break;
+  //     }
+  //   }
+
+  //   await gateway.finalize();
+
+  //   console.log(gateway.params);
+
+  //   const txs = gateway.transactions();
+
+  //   console.log("======");
+  //   console.log("Txs are sent...");
+  //   for (let tx of txs) {
+  //     const sig = await provider.sendAndConfirm(tx, [], {
+  //       skipPreflight: true,
+  //       commitment: "confirmed",
+  //     } as unknown as anchor.web3.ConfirmOptions);
+  //     console.log(sig);
+  //   }
+  //   console.log("Txs are executed");
+  //   console.log("======");
+  // });
+
+  // it("Harvest all in Genopets", async () => {
+  //   const farmId = new anchor.web3.PublicKey(
+  //     "tEAbLeDznDdQ5jvdk1cdm2qUzoYmyc6nX5FChAyAB2U"
+  //   );
+
+  //   const farm = (await genopets.infos.getFarm(
+  //     connection,
+  //     farmId
+  //   )) as genopets.FarmInfo;
+
+  //   const farmerId = await genopets.infos.getFarmerId(
+  //     farm,
+  //     provider.wallet.publicKey
+  //   );
+  //   const farmer = (await genopets.infos.getFarmer(
+  //     connection,
+  //     farmerId
+  //   )) as genopets.FarmerInfo;
+
+  //   const gateway = new GatewayBuilder(provider);
+
+  //   let depositId: anchor.web3.PublicKey;
+  //   let counter = 0;
+  //   for (let deposit of farmer.userDeposit) {
+  //     if (deposit?.isYield == true) {
+  //       depositId = deposit.depositId;
+
+  //       const harvestParams2: HarvestParams = {
+  //         protocol: SupportedProtocols.Genopets,
+  //         farmId,
+  //         type: HarvestType.WithdrawAsSgene,
+  //         farmerKey: depositId!,
+  //         mint,
+  //       };
+
+  //       await gateway.harvest(harvestParams2);
+  //       counter++;
+  //       if (counter == 6) break;
+  //     }
+  //   }
+
+  //   await gateway.finalize();
+
+  //   console.log(gateway.params);
+
+  //   const txs = gateway.transactions();
+
+  //   console.log("======");
+  //   console.log("Txs are sent...");
+  //   for (let tx of txs) {
+  //     const sig = await provider.sendAndConfirm(tx, [], {
+  //       skipPreflight: true,
+  //       commitment: "confirmed",
+  //     } as unknown as anchor.web3.ConfirmOptions);
+  //     console.log(sig);
+  //   }
+  //   console.log("Txs are executed");
+  //   console.log("======");
+  // });
 });
