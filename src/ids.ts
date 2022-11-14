ab682f08e8d8ff33c3e4edd8edb20aa73b26e8d3
Alvin Philips <alvinphilips257@gmail.com>
2022-11-14 23:47:14 +0000

    Updated Lido Adapter Program ID

--- src/ids.ts
+++ src/ids.ts
@@ -2,65 +2,34 @@ import { PublicKey } from "@solana/web3.js";
 
 // Program IDs
 
-export const GATEWAY_PROGRAM_ID = new PublicKey(
-  "GATEp6AEtXtwHABNWHKH9qeh3uJDZtZJ7YBNYzHsX3FS"
-);
+export const GATEWAY_PROGRAM_ID = new PublicKey("GATEp6AEtXtwHABNWHKH9qeh3uJDZtZJ7YBNYzHsX3FS");
 
-export const RAYDIUM_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPT1q4xG8F9m64cQyjqGe11cCXQq6vL4beY5hJavhQ5"
-);
+export const RAYDIUM_ADAPTER_PROGRAM_ID = new PublicKey("ADPT1q4xG8F9m64cQyjqGe11cCXQq6vL4beY5hJavhQ5");
 
-export const SABER_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPT4GbWTs9DXxo91YGBjNntYwLpXxn4gEbxfnUPfQoB"
-);
+export const SABER_ADAPTER_PROGRAM_ID = new PublicKey("ADPT4GbWTs9DXxo91YGBjNntYwLpXxn4gEbxfnUPfQoB");
 
-export const TULIP_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPT9nhC1asRcEB13FKymLTatqWGCuZHDznGgnakWKxW"
-);
+export const TULIP_ADAPTER_PROGRAM_ID = new PublicKey("ADPT9nhC1asRcEB13FKymLTatqWGCuZHDznGgnakWKxW");
 
-export const SOLEND_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTCXAFfJFVqcw73B4PWRZQjMNo7Q3Yj4g7p4zTiZnQ"
-);
+export const SOLEND_ADAPTER_PROGRAM_ID = new PublicKey("ADPTCXAFfJFVqcw73B4PWRZQjMNo7Q3Yj4g7p4zTiZnQ");
 
-export const NFT_FINANCE_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTyBr92sBCE1hdYBRvXbMpF4hKs17xyDjFPxopcsrh"
-);
+export const NFT_FINANCE_ADAPTER_PROGRAM_ID = new PublicKey("ADPTyBr92sBCE1hdYBRvXbMpF4hKs17xyDjFPxopcsrh");
 
-export const SERUM_PROGRAM_ID = new PublicKey(
-  "9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin"
-);
+export const SERUM_PROGRAM_ID = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
 
 export const NATIVE_SOL = new PublicKey("11111111111111111111111111111111");
 
-export const WSOL = new PublicKey(
-  "So11111111111111111111111111111111111111112"
-);
+export const WSOL = new PublicKey("So11111111111111111111111111111111111111112");
 
-export const LARIX_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTLQQ1Bwgybb2qge7QKSW7woDrhEjcLWG642qP2X4"
-);
+export const LARIX_ADAPTER_PROGRAM_ID = new PublicKey("ADPTLQQ1Bwgybb2qge7QKSW7woDrhEjcLWG642qP2X4");
 
-export const LIFINITY_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTF4WmNPebELw6UvnSVBdL7BAqs5ceg9tyrHsQfrJK"
-);
+export const LIFINITY_ADAPTER_PROGRAM_ID = new PublicKey("ADPTF4WmNPebELw6UvnSVBdL7BAqs5ceg9tyrHsQfrJK");
 
-export const ORCA_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTTyNqameXftbqsxwXhbs7v7XP8E82YMaUStPgjmU5"
-);
+export const ORCA_ADAPTER_PROGRAM_ID = new PublicKey("ADPTTyNqameXftbqsxwXhbs7v7XP8E82YMaUStPgjmU5");
 
-export const KATANA_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTwDKJTizC3V8gZXDxt5uLjJv4pBnh1nTTf9dZJnS2"
-);
+export const KATANA_ADAPTER_PROGRAM_ID = new PublicKey("ADPTwDKJTizC3V8gZXDxt5uLjJv4pBnh1nTTf9dZJnS2");
 
-export const FRANCIUM_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTax5HwQ2ZWVLmceCek8UrqMhwCy5q3SHwi8W71Kv2"
-);
+export const FRANCIUM_ADAPTER_PROGRAM_ID = new PublicKey("ADPTax5HwQ2ZWVLmceCek8UrqMhwCy5q3SHwi8W71Kv2");
 
-export const FRIKTION_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTzbsaBdXA3FqXoPHjaTjPfh9kadxxFKxonZihP1Ji"
-);
+export const FRIKTION_ADAPTER_PROGRAM_ID = new PublicKey("ADPTzbsaBdXA3FqXoPHjaTjPfh9kadxxFKxonZihP1Ji");
 
-// TODO: UPDATE THIS
-export const LIDO_ADAPTER_PROGRAM_ID = new PublicKey(
-  "ADPTzbsaBdXA3FqXoPHjaTjPfh9kadxxFKxonZihP1Ji"
-);
+export const LIDO_ADAPTER_PROGRAM_ID = new PublicKey("ADPTPxbHbEBo9A8E53P2PZnmw3ZYJuwc8ArQQkbJtqhx");
