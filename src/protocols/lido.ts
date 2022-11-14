859bd9037e913097366fb286093d2aeb882bc64d
Alvin Philips <alvinphilips257@gmail.com>
2022-11-14 23:12:36 +0000

    feat: Added withdrawal

--- src/protocols/lido.ts
+++ src/protocols/lido.ts
@@ -70,4 +70,113 @@ export class ProtocolLido implements IProtocolVault {
 
     return { txs: [txDeposit], input: payload };
   }
+
+  async withdraw(
+    params: WithdrawParams,
+    vault: IVaultInfo,
+    userKey: anchor.web3.PublicKey
+  ): Promise<{ txs: anchor.web3.Transaction[]; input: Buffer }> {
+    // Handle payload input here
+    const inputLayout = struct([u8("instruction"), nu64("amount"), u32("validatorIndex")]);
+    let payload = Buffer.alloc(PAYLOAD_SIZE);
+
+    const vaultInfo = vault as lido.VaultInfo;
+    const vaultInfoWrapper = new lido.VaultInfoWrapper(vaultInfo);
+
+    const heaviestValidator = vaultInfoWrapper.getHeaviestValidator();
+    // TODO: Not required in case of v1, so perhaps this should only be calculated in case of v2?
+    const heaviestValidatorIndex = vaultInfo.validators.entries.findIndex((value) =>
+      value.pubkey.equals(heaviestValidator.pubkey)
+    );
+
+    inputLayout.encode(
+      {
+        // The instuction is 23 if using lido v2, 2 for v1
+        instruction: vaultInfo.lidoVersion.toNumber() == 2 ? 23 : 2,
+        amount: new anchor.BN(params.withdrawAmount),
+        // Set validator index. Only used in v2
+        validatorIndex: heaviestValidatorIndex,
+      },
+      payload
+    );
+
+    // Account to temporarily hold stSOL in
+    const receivingAccount = anchor.web3.Keypair.generate();
+
+    const senderStSolAddress = await getAssociatedTokenAddress(vault.shareMint, userKey);
+
+    const bufferArrayStake = [lido.LIDO_ADDRESS.toBuffer(), Buffer.from("stake_authority")];
+    const [stakeAuthority] = anchor.web3.PublicKey.findProgramAddressSync(bufferArrayStake, lido.LIDO_PROGRAM_ID);
+
+    const validatorStakeSeeds = [
+      lido.LIDO_ADDRESS.toBuffer(),
+      heaviestValidator.pubkey.toBuffer(),
+      Buffer.from("validator_stake_account"),
+      Buffer.from(heaviestValidator.entry.stakeSeeds.begin.toArray("le", 8)),
+    ];
+
+    const [validatorStakeAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+      validatorStakeSeeds,
+      lido.LIDO_PROGRAM_ID
+    );
+
+    // Set up accounts
+    let remainingAccounts: anchor.web3.AccountMeta[];
+
+    if (vaultInfo.lidoVersion.toNumber() == 2) {
+      remainingAccounts = [
+        { pubkey: lido.LIDO_ADDRESS, isSigner: false, isWritable: true }, // 1
+        { pubkey: userKey, isSigner: true, isWritable: false }, // 2
+        { pubkey: senderStSolAddress, isSigner: false, isWritable: true }, // 3
+        { pubkey: vault.shareMint, isSigner: false, isWritable: true }, // 4
+        { pubkey: heaviestValidator.pubkey, isSigner: false, isWritable: false }, // 5
+        { pubkey: validatorStakeAccount, isSigner: false, isWritable: true }, // 6
+        { pubkey: receivingAccount.publicKey, isSigner: true, isWritable: true }, // 7
+        { pubkey: stakeAuthority, isSigner: false, isWritable: false }, // 8
+        { pubkey: vaultInfo.validatorList, isSigner: false, isWritable: true }, // 9
+        { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false }, // 10
+        { pubkey: anchor.web3.SYSVAR_CLOCK_PUBKEY, isSigner: false, isWritable: false }, // 11
+        { pubkey: anchor.web3.SystemProgram.programId, isSigner: false, isWritable: false }, // 12
+        { pubkey: anchor.web3.StakeProgram.programId, isSigner: false, isWritable: false }, // 13
+      ];
+    } else {
+      remainingAccounts = [
+        { pubkey: lido.LIDO_ADDRESS, isSigner: false, isWritable: true }, // 1
+        { pubkey: userKey, isSigner: true, isWritable: false }, // 2
+        { pubkey: senderStSolAddress, isSigner: false, isWritable: true }, // 3
+        { pubkey: vault.shareMint, isSigner: false, isWritable: true }, // 4
+        { pubkey: heaviestValidator.pubkey, isSigner: false, isWritable: false }, // 5
+        { pubkey: validatorStakeAccount, isSigner: false, isWritable: true }, // 6
+        { pubkey: receivingAccount.publicKey, isSigner: true, isWritable: true }, // 7
+        { pubkey: stakeAuthority, isSigner: false, isWritable: false }, // 8
+        { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false }, // 10
+        { pubkey: anchor.web3.SYSVAR_CLOCK_PUBKEY, isSigner: false, isWritable: false }, // 11
+        { pubkey: anchor.web3.SystemProgram.programId, isSigner: false, isWritable: false }, // 12
+        { pubkey: anchor.web3.StakeProgram.programId, isSigner: false, isWritable: false }, // 13
+      ];
+    }
+
+    // Create transaction for post-unstaking withdrawal of funds
+    // TODO: Maybe add signers?
+    const txDeactivate = anchor.web3.StakeProgram.deactivate({
+      authorizedPubkey: userKey,
+      stakePubkey: receivingAccount.publicKey,
+    });
+
+    // Handle transaction here
+    const txWithdraw = await this._gatewayProgram.methods
+      .withdraw()
+      .accounts({
+        gatewayState: this._gatewayStateKey,
+        adapterProgramId: LIDO_ADAPTER_PROGRAM_ID,
+        baseProgramId: lido.LIDO_PROGRAM_ID,
+        activityIndex: await getActivityIndex(userKey),
+        gatewayAuthority: getGatewayAuthority(),
+      })
+      .remainingAccounts(remainingAccounts)
+      .postInstructions(txDeactivate.instructions)
+      .transaction();
+
+    return { txs: [txWithdraw], input: payload };
+  }
 }
